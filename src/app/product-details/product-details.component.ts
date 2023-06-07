25a40eca15379e541b136b875980612f1aba8dbc
NirmalGit <nirsha2007@gmail.com>
2023-06-07 11:52:00 +0530

    feat: products details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
 
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+      const routeParams = this.route.snapshot.paramMap;
+  }
 }
